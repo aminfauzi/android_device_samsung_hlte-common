e6d726779e1bc574f15d145a06c8d76ac71b9d86
aminfauzi <aremean0107@gmail.com>
2020-06-22 03:27:18 +0000

    hlte-common: Built CustomDoze

--- hlte.mk
+++ hlte.mk
@@ -44,6 +44,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libshim_camera
 
+# CustomDoze
+RODUCT_PACKAGES += \
+    CustomDoze
+
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
